ede654d4bc9a16b624d493e797b3ac8b6deb1565
Mak22Khan <mak22online@gmail.com>
2023-07-25 22:47:41 +0500

    updated-static-settings

---
+++ wp-content/plugins/addon-elements-for-elementor-page-builder/elementor-addon-elements.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function plugins_url() in /Users/mak22online/Local Sites/synnestraportfolio/app/public/wp-content/plugins/addon-elements-for-elementor-page-builder/elementor-addon-elements.php:16
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/mak22online/Local Sites/synnestraportfolio/app/public/wp-content/plugins/addon-elements-for-elementor-page-builder/elementor-addon-elements.php</b> on line <b>16</b><br>
---
+++ wp-content/plugins/addon-elements-for-elementor-page-builder/inc/bootstrap.php
@@ -0,0 +1,8 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Undefined constant &quot;EAE_PATH&quot; in /Users/mak22online/Local Sites/synnestraportfolio/app/public/wp-content/plugins/addon-elements-for-elementor-page-builder/inc/bootstrap.php:261
+Stack trace:
+#0 /Users/mak22online/Local Sites/synnestraportfolio/app/public/wp-content/plugins/addon-elements-for-elementor-page-builder/inc/bootstrap.php(29): WTS_EAE\Plugin-&gt;autoload('WTS_EAE\\Classes...')
+#1 /Users/mak22online/Local Sites/synnestraportfolio/app/public/wp-content/plugins/addon-elements-for-elementor-page-builder/inc/bootstrap.php(21): WTS_EAE\Plugin-&gt;__construct()
+#2 /Users/mak22online/Local Sites/synnestraportfolio/app/public/wp-content/plugins/addon-elements-for-elementor-page-builder/inc/bootstrap.php(489): WTS_EAE\Plugin::get_instance()
+#3 {main}
+  thrown in <b>/Users/mak22online/Local Sites/synnestraportfolio/app/public/wp-content/plugins/addon-elements-for-elementor-page-builder/inc/bootstrap.php</b> on line <b>261</b><br>
